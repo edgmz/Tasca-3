4a329c515a372b6dfc9bb756b43cacd012a64b8d
edgmz <edgribcas@alu.edu.gva.es>
2026-03-11 12:28:10 +0100

    añadido Operaciones Mentales

--- Algoritmes/src/es/iesmz/ed/algoritmes/Main.java
+++ Algoritmes/src/es/iesmz/ed/algoritmes/Main.java
@@ -1,7 +1,11 @@
 package es.iesmz.ed.algoritmes;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        calculMental c = new calculMental("15 + 8 - 7 + 10 - 3");
 
+        System.out.println(c.calcula());
     }
 }
--- Algoritmes/src/es/iesmz/ed/algoritmes/calculMental.java
+++ Algoritmes/src/es/iesmz/ed/algoritmes/calculMental.java
@@ -1,5 +1,34 @@
 package es.iesmz.ed.algoritmes;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class calculMental {
-    
+    private String s;
+
+    public calculMental(String s) {
+        this.s = s;
+    }
+
+    public ArrayList<Integer> calcula() {
+        String[] partes = this.s.trim().split(" ");
+        ArrayList<Integer> resultados = new ArrayList<Integer>();
+
+        int resultado = Integer.parseInt(partes[0]);
+
+        for (int i = 1; i < partes.length; i+=2) {
+            switch (partes[i]) {
+                case "+" -> {
+                    resultado += Integer.parseInt(partes[i + 1]);
+                    resultados.add(resultado);
+                }
+                case "-" -> {
+                    resultado -= Integer.parseInt(partes[i + 1]);
+                    resultados.add(resultado);
+                }
+            }
+        }
+
+        return resultados;
+    }
 }
